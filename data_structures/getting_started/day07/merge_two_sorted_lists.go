30fdcb2cfac2bbf631c0623573535db49fcdb281
Jormin <jorminxee@gmail.com>
2021-10-21 10:23:12 +0800

    :sparkles: feat(1.0.0): 21. 合并两个有序链表
    Signed-off-by: Jormin <jorminxee@gmail.com>

---
+++ data_structures/getting_started/day07/merge_two_sorted_lists.go
@@ -0,0 +1,40 @@
+package day07
+
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+// 示例 1：
+// 输入：l1 = [1,2,4], l2 = [1,3,4]
+// 输出：[1,1,2,3,4,4]
+//
+// 示例 2：
+// 输入：l1 = [], l2 = []
+// 输出：[]
+//
+// 示例 3：
+// 输入：l1 = [], l2 = [0]
+// 输出：[0]
+//
+// 提示：
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+//
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// mergeTwoLists 合并两个有序链表
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l2.Next, l1)
+	return l2
+}
