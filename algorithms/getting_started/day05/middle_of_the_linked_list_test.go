c26397497deb293b89982d8327331dcbf9516b9a
Jormin <jorminxee@gmail.com>
2021-10-26 13:52:42 +0800

    :sparkles: feat(1.0.0): day05 - 876. 链表的中间结点
    Signed-off-by: Jormin <jorminxee@gmail.com>

---
+++ algorithms/getting_started/day05/middle_of_the_linked_list_test.go
@@ -0,0 +1,98 @@
+package day05
+
+import (
+	"reflect"
+	"testing"
+)
+
+// TestMiddleNode 测试链表的中间结点
+func TestMiddleNode(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "01",
+			args: args{head: nil},
+			want: nil,
+		},
+		{
+			name: "02",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val:  5,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+		{
+			name: "03",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val: 5,
+									Next: &ListNode{
+										Val:  6,
+										Next: nil,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 4,
+				Next: &ListNode{
+					Val: 5,
+					Next: &ListNode{
+						Val:  6,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := middleNode(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("middleNode() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
