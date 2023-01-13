e4aa184c06373b5be748f380f01a248a65c04fb4
Jormin <jorminxee@gmail.com>
2023-01-14 02:44:36 +0800

    commit

---
+++ data_structures/base/day08/group_anagrams.go
@@ -0,0 +1,31 @@
+package day08
+
+// 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+//
+// 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母都恰好只用一次。
+//
+// 示例 1:
+// 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+// 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+//
+// 示例 2:
+// 输入: strs = [""]
+// 输出: [[""]]
+//
+// 示例 3:
+// 输入: strs = ["a"]
+// 输出: [["a"]]
+//
+// 提示：
+// 1 <= strs.length <= 104
+// 0 <= strs[i].length <= 100
+// strs[i]仅包含小写字母
+//
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/group-anagrams
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// groupAnagrams 字母异位词分组
+func groupAnagrams(strs []string) [][]string {
+	return nil
+}
